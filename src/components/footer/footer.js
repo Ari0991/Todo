fc554b9503acda63ef6754722c41abe66e801f21
Tatiana Vostrikova <103586136+Ari0991@users.noreply.github.com>
2023-04-07 12:45:43 +0700

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-import './footer.css';
+import './footer.css'
 
 export default class Footer extends Component {
   buttons = [
     { name: 'all', text: 'All' },
     { name: 'active', text: 'Active' },
     { name: 'completed', text: 'Completed' },
-  ];
+  ]
 
   render() {
-    const { allCount, doneCount, filter, onFilterChange, onClearCompleted } = this.props;
+    const { allCount, doneCount, filter, onFilterChange, onClearCompleted } = this.props
 
     const buttons = this.buttons.map(({ name, text, type }) => {
-      const isActive = filter === name;
-      const classText = isActive ? 'selected' : null;
+      const isActive = filter === name
+      const classText = isActive ? 'selected' : null
       return (
         <li key={name}>
           <button type={type} name={name} className={classText} onClick={() => onFilterChange(name)}>
             {text}
           </button>
         </li>
-      );
-    });
+      )
+    })
     return (
       <footer className="footer">
         <div className="todo-count">{allCount - doneCount} items left</div>
@@ -32,19 +32,19 @@ export default class Footer extends Component {
           Clear completed
         </button>
       </footer>
-    );
+    )
   }
 
   static defaultProps = {
     allCount: 0,
     doneCount: 0,
     filter: 'all',
-  };
+  }
   static propTypes = {
     allCount: PropTypes.number,
     doneCount: PropTypes.number,
     filter: PropTypes.string,
     onFilterChange: PropTypes.func.isRequired,
     onClearCompleted: PropTypes.func.isRequired,
-  };
+  }
 }
