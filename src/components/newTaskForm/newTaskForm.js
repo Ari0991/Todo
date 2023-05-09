4006c64aedd9d3ed06a978dd4ca51b849b714201
Tatiana Vostrikova <t.belgorodskaya@yandex.ru>
2023-05-09 10:22:51 +0700

    add hooks

--- src/components/newTaskForm/newTaskForm.js
+++ src/components/newTaskForm/newTaskForm.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Alert } from 'antd'
 
 import './newTaskForm.css'
 
-export default class NewTaskForm extends Component {
-  state = {
-    value: '',
-    minutes: '',
-    seconds: '',
-    correctTime: true,
+const NewTaskForm = ({ addItem }) => {
+  const [value, setValue] = useState('')
+  const [minutes, setMinutes] = useState('')
+  const [seconds, setSeconds] = useState('')
+  const [correctTime, setCorrectTime] = useState(true)
+
+  const changeState = (evt) => {
+    setValue(evt.target.value)
   }
 
-  changeState = (evt) => {
-    this.setState({ value: evt.target.value })
+  const changeMin = (evt) => {
+    setMinutes(trimValue(evt.target.value))
   }
 
-  changeMin = (evt) => {
-    this.setState({ minutes: this.trimValue(evt.target.value) })
+  const changeSec = (evt) => {
+    setSeconds(trimValue(evt.target.value))
   }
 
-  changeSec = (evt) => {
-    this.setState({ seconds: this.trimValue(evt.target.value) })
-  }
-
-  timeCheck = (minutes, seconds) => {
+  const timeCheck = (minutes, seconds) => {
     const min = Number(minutes)
     const sec = Number(seconds)
 
@@ -36,76 +34,76 @@ export default class NewTaskForm extends Component {
     return true
   }
 
-  trimValue = (value) => {
+  const trimValue = (value) => {
     return value.trim()
   }
 
-  submitValue = (evt) => {
+  const submitValue = (evt) => {
     evt.preventDefault()
-    const { value, minutes, seconds } = this.state
-    const { addItem } = this.props
-    const verify = this.timeCheck(minutes, seconds)
+    const verify = timeCheck(minutes, seconds)
 
     if (!verify) {
-      return this.setState({ correctTime: false })
-    } else if (verify && this.trimValue(value) !== '') {
+      return setCorrectTime(false)
+    } else if (verify && trimValue(value) !== '') {
       addItem(value, minutes, seconds)
-      this.setState({ value: '', minutes: '', seconds: '', correctTime: true })
+      setValue('')
+      setSeconds('')
+      setMinutes('')
+      setCorrectTime(true)
     }
   }
 
-  render() {
-    const { correctTime } = this.state
-    const isCorrectTime = correctTime ? null : (
-      <Alert
-        closable
-        message="Неверный формат данных"
-        description="Пожалуйста, введите мин. и сек. в диапазоне от 0 до 60, общее время не более 60 мин."
-        type="warning"
-      />
-    )
+  const isCorrectTime = correctTime ? null : (
+    <Alert
+      closable
+      message="Неверный формат данных"
+      description="Пожалуйста, введите мин. и сек. в диапазоне от 0 до 60, общее время не более 60 мин."
+      type="warning"
+    />
+  )
 
-    return (
-      <header className="header">
-        <h1>Todos</h1>
-        {isCorrectTime}
-        <form className="new-todo-form" onSubmit={this.submitValue}>
-          <input
-            type="text"
-            className="new-todo edit"
-            placeholder="What needs to be done?"
-            autoFocus
-            value={this.state.value}
-            onChange={this.changeState}
-            required
-          />
-          <input
-            className="new-todo-form__timer"
-            placeholder="Min"
-            autoFocus
-            maxLength="2"
-            onChange={this.changeMin}
-            value={this.state.minutes}
-            pattern="[0-9]{1,2}"
-            required
-          />
-          <input
-            className="new-todo-form__timer"
-            placeholder="Sec"
-            autoFocus
-            maxLength="2"
-            onChange={this.changeSec}
-            value={this.state.seconds}
-            pattern="[0-9]{1,2}"
-            required
-          />
-          <button type="submit"></button>
-        </form>
-      </header>
-    )
-  }
-
-  static propTypes = {
-    addItem: PropTypes.func.isRequired,
-  }
+  return (
+    <header className="header">
+      <h1>Todos</h1>
+      {isCorrectTime}
+      <form className="new-todo-form" onSubmit={submitValue}>
+        <input
+          type="text"
+          className="new-todo edit"
+          placeholder="What needs to be done?"
+          autoFocus
+          value={value}
+          onChange={changeState}
+          required
+        />
+        <input
+          className="new-todo-form__timer"
+          placeholder="Min"
+          autoFocus
+          maxLength="2"
+          onChange={changeMin}
+          value={minutes}
+          pattern="[0-9]{1,2}"
+          required
+        />
+        <input
+          className="new-todo-form__timer"
+          placeholder="Sec"
+          autoFocus
+          maxLength="2"
+          onChange={changeSec}
+          value={seconds}
+          pattern="[0-9]{1,2}"
+          required
+        />
+        <button type="submit"></button>
+      </form>
+    </header>
+  )
 }
+
+NewTaskForm.propTypes = {
+  addItem: PropTypes.func.isRequired,
+}
+
+export default NewTaskForm
